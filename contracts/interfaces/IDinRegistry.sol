713a6fd841aa2f6154d9cee2e98a8b9dbc5e915f
haryunio <hr4574@naver.com>
2025-08-25 15:44:03 +0900

    feat: add getYieldRouter function to IDinRegistry interface

--- contracts/interfaces/IDinRegistry.sol
+++ contracts/interfaces/IDinRegistry.sol
@@ -6,6 +6,7 @@ interface IDinRegistry {
     function getDINToken() external view returns (address);
     function getProductCatalog() external view returns (address);
     function getFeeTreasury() external view returns (address);
+    function getYieldRouter() external view returns (address);
     function getProtocolFeeBps() external view returns (uint256);
     function getContractAddress(bytes32 identifier) external view returns (address);
 }
